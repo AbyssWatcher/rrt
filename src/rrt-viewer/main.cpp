48fd678ef8d5321347337747392b2f8ec30f0209
Justin Buchanan <justbuchanan@gmail.com>
2017-01-18 22:00:34 -0800

    got basic qml loading to work

--- src/rrt-viewer/main.cpp
+++ src/rrt-viewer/main.cpp
@@ -1,13 +1,34 @@
 
+#include <QQmlApplicationEngine>
+#include <QQuickView>
+#include <QtQuick>
 #include <QtWidgets>
+#include <iostream>
 #include "MainWindow.hpp"
+
+using namespace std;
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     app.setOrganizationName("RoboJackets");
     app.setApplicationName("Interactive RRT");
-    MainWindow mainWin;
-    mainWin.show();
+
+    // load qml
+    QQmlApplicationEngine engine(nullptr);
+    engine.rootContext()->setContextProperty("main", &engine);
+    engine.addImportPath("./src/rrt-viewer");  // TODO: fix
+    engine.load("./src/rrt-viewer/main.qml");
+
+    // get reference to main window from qml
+    auto r = engine.rootObjects()[0];
+    cout << r->objectName().toStdString() << endl;
+    auto win = static_cast<QQuickView*>(r);
+    if (!win) {
+        cerr << "Failed to load qml" << endl;
+        exit(1);
+    }
+
+    win->show();
 
     return app.exec();
 }
---
+++ src/rrt-viewer/main.qml
@@ -0,0 +1,13 @@
+import QtQuick 2.7
+import QtQuick.Window 2.1
+import QtQuick.Controls 2.0
+import QtQuick.Dialogs 1.2
+import QtQuick.Layouts 1.2
+import QtCharts 1.2
+
+ApplicationWindow {
+    title: "Arm Control"
+
+    width: 200
+    height: 200
+}
